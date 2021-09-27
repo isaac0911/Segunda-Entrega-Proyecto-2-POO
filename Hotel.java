6775f42907bc5251801bd3ed067ebe7141d71f66
stndred1 <87286644+stndred1@users.noreply.github.com>
2021-09-26 18:37:13 -0600

    Hotel  class java POO prototipo v2.0

---
+++ Hotel.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+
+public class Hotel{
+	
+	private String Nombre;
+	private String Direccion;
+	private float PrecioNoche;
+	private float Estrellas;
+	private String SitioWeb;
+	
+	public Hotel(ArrayList<String> DatosNuevoHotel){
+		
+		Nombre = DatosNuevoHotel.get(0);
+		Direccion = DatosNuevoHotel.get(4);
+		PrecioNoche = Float.parseFloat(DatosNuevoHotel.get(6));
+		Estrellas = Float.parseFloat(DatosNuevoHotel.get(7));
+		SitioWeb = DatosNuevoHotel.get(5);
+		
+	}
+	
+	public String toString(){
+		
+		return "\n\nLos datos del " + Nombre + " son:\n\t-Direccion: " + Direccion + "\n\t-Precio por noche: Desde Q" + PrecioNoche + "\n\t-Calificacion: " + Estrellas + " estrellas de 5\n\t-SitioWeb: " + SitioWeb;
+	}
+	
+	public static ArrayList<ArrayList<String>> FiltrarHotelesPorRegion (String Nombreregion, ArrayList<ArrayList<String>> DatosHoteles){
+		
+		ArrayList<String> Regiones = new ArrayList<String>();
+		
+		for (int i=0; i<DatosHoteles.size(); i++){
+			ArrayList<String> DatosHotel = DatosHoteles.get(i);
+			Regiones.add(DatosHotel.get(1));
+		}
+
+		ArrayList<Integer> PosicionesRegion = new ArrayList<Integer>();
+		
+		for (int i=0;i<Regiones.size(); i++){
+			if (Nombreregion.equals(Regiones.get(i))){
+				PosicionesRegion.add(i);
+			}
+		}
+		
+		ArrayList<ArrayList<String>> HotelesDeUnaRegion = new ArrayList<ArrayList<String>>();
+		
+		for (int i=0; i<PosicionesRegion.size(); i++){
+			int posicion = PosicionesRegion.get(i);
+			ArrayList<String> DatosHotel = DatosHoteles.get(posicion);
+			HotelesDeUnaRegion.add(DatosHotel);
+		}
+		
+		return HotelesDeUnaRegion;
+		
+	}
+	
+	public static ArrayList<ArrayList<String>> FiltrarHotelesPorMonto(float MontoMax, ArrayList<ArrayList<String>> HotelesAFiltrar){
+		
+		ArrayList<Float> Precios = new ArrayList<Float>();
+		
+		for (int i=0; i<HotelesAFiltrar.size(); i++){
+			ArrayList<String> DatosHotel = HotelesAFiltrar.get(i);
+			Precios.add(Float.parseFloat(DatosHotel.get(6)));
+		}
+		
+		ArrayList<Integer> PosicionesPreciosQueCumplen = new ArrayList<Integer>();
+		
+		for (int i=0;i<Precios.size(); i++){
+			if (Precios.get(i) <= MontoMax){
+				PosicionesPreciosQueCumplen.add(i);
+			}
+		}
+		
+		ArrayList<ArrayList<String>> DatosHotelesElegidos = new ArrayList<ArrayList<String>>();
+		
+		for (int i=0; i<PosicionesPreciosQueCumplen.size(); i++){
+			int posicion = PosicionesPreciosQueCumplen.get(i);
+			ArrayList<String> DatosHotel = HotelesAFiltrar.get(posicion);
+			DatosHotelesElegidos.add(DatosHotel);
+		}
+		
+		while (DatosHotelesElegidos.size()>10){
+			DatosHotelesElegidos.remove(0);
+		}
+		
+		return DatosHotelesElegidos;
+		
+	}
+}
